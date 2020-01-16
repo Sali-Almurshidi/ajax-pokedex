6bac881cd15b1f3c67b169e83be6880b476d491d
sali <salialmurshidi@gmail.com>
2020-01-16 12:11:44 +0100

    clear the page for every search

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-
-let countID = 0;
-
+let elementsFatherDiv  = null;
 // function to put the input from the user in fetch
-function getArray(userInput) {
+function getArray(userInput, countID) {
     fetch("https://pokeapi.co/api/v2/pokemon/" + userInput + "/")
         .then((response) => {
             return response.json();
         })
         .then((myJson) => {
-            getInformation(myJson);
+            getInformation(myJson, countID);
             console.log(myJson);
         });
 }
 
 // get information from  getArray function
-function getInformation(infoArray) {
+function getInformation(infoArray, countID) {
     let pokemonArray = infoArray;
     // let to get the pokemon id
     let inputID = pokemonArray.id;
@@ -31,13 +29,13 @@ function getInformation(infoArray) {
     // get the url from species to check if there is previous evolution
     let speciesURL = pokemonArray.species.url;
     // to send the data to printInfo
-    printInfo(inputID, inputName, imgSprite, movesArray);
+    printInfo(inputID, inputName, imgSprite, movesArray, countID);
     // send the URL to the fetch to get the previous evolution
-    sendURL(speciesURL);
+    sendURL(speciesURL, countID);
 }
 
 // function to print the data on the screen
-function printInfo(inputID, inputName, imgSprite, movesArray) {
+function printInfo(inputID, inputName, imgSprite, movesArray, countID) {
 
     // create a new div element
     let elementsDiv = document.createElement("div");
@@ -48,21 +46,21 @@ function printInfo(inputID, inputName, imgSprite, movesArray) {
     let pokemonID = document.createElement("h2");
     let idID = "h2ID" + countID;
     pokemonID.setAttribute("id", idID);
-    pokemonID.innerHTML = "Pokemon ID "+ inputID;
+    pokemonID.innerHTML = "Pokemon ID " + inputID;
     elementsDiv.appendChild(pokemonID);
 
     // creat h2 for the name
     let pokemonName = document.createElement("h2");
     let idName = "h2Name" + countID;
     pokemonName.setAttribute("id", idName);
-    pokemonName.innerHTML = "Pokemon Name "+inputName;
+    pokemonName.innerHTML = "Pokemon Name " + inputName;
     elementsDiv.appendChild(pokemonName);
 
     // creat img element
     let pokemonImg = document.createElement("img");
     let idImg = "pokemonImg" + countID;
     pokemonImg.setAttribute("id", idImg);
-    pokemonImg.src = imgSprite ;
+    pokemonImg.src = imgSprite;
     console.log(imgSprite);
     elementsDiv.appendChild(pokemonImg);
 
@@ -72,11 +70,11 @@ function printInfo(inputID, inputName, imgSprite, movesArray) {
     movesUl.setAttribute("id", idUl);
     movesUl.innerHTML = "The moves for this pokemon ";
     // creat the moves in ul
-    for(i=0 ; i<4 ; i++){
+    for (i = 0; i < 4; i++) {
         let moveLi = document.createElement("li");
-        let idLi = "moveLi" + countID + i ;
+        let idLi = "moveLi" + countID + i;
         moveLi.setAttribute("id", idLi);
-        moveLi.innerHTML =  movesArray[i];
+        moveLi.innerHTML = movesArray[i];
         movesUl.appendChild(moveLi);
     }
     elementsDiv.appendChild(movesUl);
@@ -85,11 +83,11 @@ function printInfo(inputID, inputName, imgSprite, movesArray) {
     let hr = document.createElement("hr");
     elementsDiv.appendChild(hr);
     // add this div to the body
-    document.body.appendChild(elementsDiv);
+    elementsFatherDiv.appendChild(elementsDiv);
 }
 
 // function to get the previous evolve if there is one
-function sendURL(speciesURL) {
+function sendURL(speciesURL, countID) {
     fetch(speciesURL)
         .then((response) => {
             return response.json();
@@ -98,27 +96,44 @@ function sendURL(speciesURL) {
             //console.log(mySecondJson.evolves_from_species.name);
             if (mySecondJson.evolves_from_species == null) {
                 console.log("done");
-/*                // create a new div element
-                let elementsDivForEnd = document.createElement("div");
-                elementsDivForEnd.setAttribute("id", "divEnd");
+                /*                // create a new div element
+                                let elementsDivForEnd = document.createElement("div");
+                                elementsDivForEnd.setAttribute("id", "divEnd");
 
-                //create a h2 for end
-                let pokemonEnd = document.createElement("h2");
-                pokemonEnd.innerHTML = "There is no e ";
-                elementsDivForEnd.appendChild(pokemonEnd);
+                                //create a h2 for end
+                                let pokemonEnd = document.createElement("h2");
+                                pokemonEnd.innerHTML = "There is no e ";
+                                elementsDivForEnd.appendChild(pokemonEnd);
 
-                document.body.appendChild(elementsDivForEnd);*/
-
+                                document.body.appendChild(elementsDivForEnd);*/
+                countID = 0;
             } else {
                 countID++;
-                getArray(mySecondJson.evolves_from_species.name);
+                getArray(mySecondJson.evolves_from_species.name, countID);
             }
         });
 }
 
 //addEventListener to the button to get the value from the user
 document.getElementById("callApi").addEventListener("click", function () {
+    let countID = 0;
+    removeElement();
     let userInput = document.getElementById("PokemonName").value;
-    getArray(userInput);
+    addFatherDiv();
+    getArray(userInput, countID);
 });
+
+function addFatherDiv() {
+    elementsFatherDiv = document.createElement("div");
+    elementsFatherDiv.setAttribute("id", "fatherDiv");
+    document.body.appendChild(elementsFatherDiv);
+}
+
+// function to remove the father div
+function removeElement() {
+    if (elementsFatherDiv != null){
+        let element = document.getElementById("fatherDiv");
+        element.parentNode.removeChild(element);
+    }
+}
 
